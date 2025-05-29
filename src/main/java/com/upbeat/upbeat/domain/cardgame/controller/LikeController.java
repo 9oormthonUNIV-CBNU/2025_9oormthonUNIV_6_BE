4aad8b585cbf7ce2b6bb0249c17fd81693b93ccc
Sein8572 <lemon007878@gmail.com>
2025-05-30 02:12:20 +0900

    chore: 컨트롤러 코드 일부 수정

--- src/main/java/com/upbeat/upbeat/domain/cardgame/controller/LikeController.java
+++ src/main/java/com/upbeat/upbeat/domain/cardgame/controller/LikeController.java
@@ -4,6 +4,10 @@ import com.upbeat.upbeat.domain.cardgame.dto.request.LikeRequestDto;
 import com.upbeat.upbeat.domain.cardgame.dto.response.LikeResponseDto;
 import com.upbeat.upbeat.domain.cardgame.service.LikeService;
 import com.upbeat.upbeat.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,23 +17,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 
+@Tag(name = "answer-like-controller")
 @RestController
 @Slf4j
+@RequestMapping("/api/cards")
+@RequiredArgsConstructor
 public class LikeController {
     @Autowired
     private LikeService likeService;
 
     //좋아요 증가
-    @PostMapping("/api/cards/{cardId}/like")
-    public ResponseEntity<LikeResponseDto> IncreaseLikes(@RequestBody LikeRequestDto dto){
-        LikeResponseDto responseDto = likeService.create(dto);
+    @Operation(summary = "좋아요 증가")
+    @PostMapping("/{cardId}/like")
+    public ResponseEntity<LikeResponseDto> increaseLike(@Valid @RequestBody LikeRequestDto dto, @AuthenticationPrincipal CustomUserDetails userDetails){
+        Long userId = userDetails.getUser().getId();
+        LikeResponseDto responseDto = likeService.create(dto, userId);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     //좋아요 취소
-    //(AnswerId, UserId 받아오는 방법 수정)
-    @DeleteMapping("/api/cards/{cardId}/likeCancel/{answerId}")
-    public ResponseEntity<LikeResponseDto> 함수명(@PathVariable BigInteger cardId, @PathVariable BigInteger answerId, @AuthenticationPrincipal CustomUserDetails userDetails){
+    @Operation(summary = "좋아요 취소")
+    @DeleteMapping("/{cardId}/likeCancel/{answerId}")
+    public ResponseEntity<LikeResponseDto> cancelLike(@PathVariable Long cardId, @PathVariable Long answerId, @AuthenticationPrincipal CustomUserDetails userDetails){
         Long userId = userDetails.getUser().getId();
         LikeResponseDto responseDto = likeService.delete(answerId,userId);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
--- src/main/java/com/upbeat/upbeat/domain/cardgame/controller/StrategyController.java
+++ src/main/java/com/upbeat/upbeat/domain/cardgame/controller/StrategyController.java
@@ -4,27 +4,32 @@ import com.upbeat.upbeat.domain.cardgame.dto.request.StrategyRequestDto;
 import com.upbeat.upbeat.domain.cardgame.dto.response.StrategyResponseDto;
 import com.upbeat.upbeat.domain.cardgame.service.StrategyService;
 import com.upbeat.upbeat.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 
+@Tag(name = "answer-strategy-controller")
 @RestController
 @Slf4j
+@RequestMapping("/api/cards")
+@RequiredArgsConstructor
 public class StrategyController {
     @Autowired
     private StrategyService strategyService;
 
     //전략 생성
-    @PostMapping("/api/cards/{cardId}/strategy")
-    public ResponseEntity<StrategyResponseDto> CreateStrategy(@PathVariable BigInteger cardId, @RequestBody StrategyRequestDto strategyDTO,@AuthenticationPrincipal CustomUserDetails userDetails){
+    @Operation(summary = "전략 생성")
+    @PostMapping("/{cardId}/strategy")
+    public ResponseEntity<StrategyResponseDto> CreateStrategy(@PathVariable Long cardId, @Valid @RequestBody StrategyRequestDto strategyDTO, @AuthenticationPrincipal CustomUserDetails userDetails){
         Long userId = userDetails.getUser().getId();
         StrategyResponseDto created = strategyService.create(cardId,strategyDTO,userId);
         return ResponseEntity.status(HttpStatus.OK).body(created);
