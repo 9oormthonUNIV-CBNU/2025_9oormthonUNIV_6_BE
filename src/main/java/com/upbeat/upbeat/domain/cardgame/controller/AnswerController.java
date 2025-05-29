407595fb3c10e707f593e6ab81c4941013f792d6
Sein8572 <lemon007878@gmail.com>
2025-05-30 02:12:20 +0900

    chore: 컨트롤러 코드 일부 수정

--- src/main/java/com/upbeat/upbeat/domain/cardgame/controller/AnswerController.java
+++ src/main/java/com/upbeat/upbeat/domain/cardgame/controller/AnswerController.java
@@ -4,27 +4,32 @@ import com.upbeat.upbeat.domain.cardgame.dto.request.AnswerRequestDto;
 import com.upbeat.upbeat.domain.cardgame.dto.response.AnswerResponseDto;
 import com.upbeat.upbeat.domain.cardgame.service.AnswerService;
 import com.upbeat.upbeat.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 
+@Tag(name = "card-answer-controller")
 @RestController
 @Slf4j
+@RequestMapping("/api/cards")
+@RequiredArgsConstructor
 public class AnswerController {
     @Autowired
     private AnswerService answerService;
 
     //답변 생성
-    @PostMapping("/api/cards/{cardId}/answer") //url 변경
-    public ResponseEntity<AnswerResponseDto> CreateAnswer(@PathVariable BigInteger cardId, @RequestBody AnswerRequestDto answerDTO,@AuthenticationPrincipal CustomUserDetails userDetails){
+    @Operation(summary = "답변 생성")
+    @PostMapping("/{cardId}/answer")
+    public ResponseEntity<AnswerResponseDto> CreateAnswer(@PathVariable Long cardId, @Valid @RequestBody AnswerRequestDto answerDTO, @AuthenticationPrincipal CustomUserDetails userDetails){
         Long userId = userDetails.getUser().getId();
         AnswerResponseDto created = answerService.create(cardId,answerDTO,userId);
         return ResponseEntity.status(HttpStatus.OK).body(created);
--- src/main/java/com/upbeat/upbeat/domain/cardgame/controller/CardController.java
+++ src/main/java/com/upbeat/upbeat/domain/cardgame/controller/CardController.java
@@ -5,15 +5,16 @@ import com.upbeat.upbeat.domain.cardgame.dto.response.CardResponseDto;
 import com.upbeat.upbeat.domain.cardgame.dto.response.card_strategies_answers.CSARDtosAnswerDto;
 import com.upbeat.upbeat.domain.cardgame.dto.response.card_strategies_answers.CSARDtosStrategyDto;
 import com.upbeat.upbeat.domain.cardgame.dto.response.card_strategies_answers.CardStrategiesAnswersResponseDto;
-import com.upbeat.upbeat.domain.cardgame.dto.response.cards_and_checkeds.CACRDtosCardDto;
-import com.upbeat.upbeat.domain.cardgame.dto.response.cards_and_checkeds.CACRDtosCheckedCardDto;
-import com.upbeat.upbeat.domain.cardgame.dto.response.cards_and_checkeds.CardsAndCheckedsResponseDto;
+import com.upbeat.upbeat.domain.cardgame.dto.response.RandomCardsResponseDto;
 import com.upbeat.upbeat.domain.cardgame.entity.Card;
 import com.upbeat.upbeat.domain.cardgame.service.AnswerService;
 import com.upbeat.upbeat.domain.cardgame.service.CardService;
-import com.upbeat.upbeat.domain.cardgame.service.CheckedCardService;
 import com.upbeat.upbeat.domain.cardgame.service.StrategyService;
 import com.upbeat.upbeat.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,47 +25,45 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigInteger;
 import java.util.List;
 
+@Tag(name = "card-controller")
 @RestController
+@RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/cards")
 public class CardController {
     @Autowired
     private CardService cardService;
     @Autowired
     private AnswerService answerService;
     @Autowired
-    private CheckedCardService checkedCardService;
-    @Autowired
     private StrategyService strategyService;
 
     //카드 생성
-    @PostMapping("/api/cards")
-    public ResponseEntity<CardResponseDto> CreateCard(@RequestBody CardRequestDto dto, @AuthenticationPrincipal CustomUserDetails userDetails){
+    @Operation(summary = "카드 생성")
+    @PostMapping("")
+    public ResponseEntity<CardResponseDto> CreateCard(@Valid @RequestBody CardRequestDto dto, @AuthenticationPrincipal CustomUserDetails userDetails){
         Long userId = userDetails.getUser().getId();
         CardResponseDto card = cardService.create(dto,userId);
         return ResponseEntity.status(HttpStatus.OK).body(card);
     }
 
     //6개 랜덤 카드
-    @GetMapping("/api/cards/show")
-    public ResponseEntity<CardsAndCheckedsResponseDto> GetRandomCards(@AuthenticationPrincipal CustomUserDetails userDetails){
-        Long userId = userDetails.getUser().getId();
-        List<CACRDtosCardDto> cards = cardService.showRandom();
-        List<CACRDtosCheckedCardDto> checkedCards = checkedCardService.response(userId);
-        return ResponseEntity.status(HttpStatus.OK).body(new CardsAndCheckedsResponseDto(cards,checkedCards));
+    @Operation(summary = "6개 랜덤 카드 추출")
+    @GetMapping("/cardinfo")
+    public ResponseEntity<RandomCardsResponseDto> GetRandomCards(){
+        List<Long> cards = cardService.showRandom();
+        return ResponseEntity.status(HttpStatus.OK).body(new RandomCardsResponseDto(cards));
     }
 
     //카드&답변 보기
-    //(UserId 받아오는 방법 수정)
-    @GetMapping("/api/cards/{cardId}/show/{userId}")
-    public ResponseEntity<CardStrategiesAnswersResponseDto> GetCardWithAnswers(@PathVariable("cardId") BigInteger cardId, @PathVariable Long userId){
-
+    @Operation(summary = "카드&답변 열람")
+    @GetMapping("/{cardId}/show")
+    public ResponseEntity<CardStrategiesAnswersResponseDto> GetCardWithAnswers(@PathVariable("cardId") Long cardId){
         Card card = cardService.show(cardId);
         if (card == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         List<CSARDtosStrategyDto> strategies = strategyService.show(cardId);
         List<CSARDtosAnswerDto> answers = answerService.show(cardId);
-        if (checkedCardService.isExist(userId, cardId) == null) {
-            checkedCardService.create(userId, cardId);
-        }
+
         return ResponseEntity.status(HttpStatus.OK).body(new CardStrategiesAnswersResponseDto(card, strategies,answers));
     }
 }
